152fc669a421ccc45d70c0a55d40c646b53b4ef4
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-09-22 10:46:14 -0700

    Network utils

---
+++ app/src/main/java/com/codepath/nytimessearch/utils/NetworkUtil.java
@@ -0,0 +1,32 @@
+package com.codepath.nytimessearch.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+
+/**
+ * Created by vidhya on 9/22/17.
+ */
+
+public class NetworkUtil {
+
+    private Boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    public boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        } catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+        return false;
+    }
+}
